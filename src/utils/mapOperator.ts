666ee8d2879cc8454981154f96598817754248e3
Poranut Chollavorn <pipech@gmail.com>
2023-12-21 09:09:30 +0700

    feat: add map operator

--- src/utils/mapOperator.ts
+++ src/utils/mapOperator.ts
@@ -1,15 +1,35 @@
 import { CrudOperators } from "@refinedev/core";
 
 export const mapOperator = (operator: CrudOperators): string => {
-    switch (operator) {
-        case "ne":
-        case "gte":
-        case "lte":
-            return `_${operator}`;
-        case "contains":
-            return "_like";
-        case "eq":
-        default:
-            return "";
-    }
+    const operatorsMap = {
+        eq: "=",
+        ne: "!=",
+        lt: "<",
+        gt: ">",
+        lte: "<=",
+        gte: ">=",
+        in: "in",
+        nin: "not in",
+        contains: "like",
+        ncontains: "not like",
+        between: "between",
+    };
+
+    const mappedOperator = operatorsMap[operator];
+
+    const notImplementedOperators = [
+        "containss",
+        "ncontainss",
+        "nbetween",
+        "null",
+        "nnull",
+        "startswith",
+        "nstartswith",
+        "startswiths",
+        "nstartswiths",
+        "endswith",
+        "nendswith",
+        "endswiths",
+        "nendswiths",
+    ];
 };
